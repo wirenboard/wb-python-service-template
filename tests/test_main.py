b8ae2a64d68bc8a88da9bfdedd759d70fffb2d6d
Валерий <trofimof09@mail.ru>
2026-07-31 17:01:43 +0300

    refactor: annotate types and unify docstrings in the template

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 from wb.python_service_template import main
 
 
-def test_missing_config_returns_config_error():
+def test_missing_config_returns_config_error() -> None:
     exit_code = main.main(["wb-foo", "-c", "/nonexistent/does-not-exist.conf"])
     assert exit_code == main.EXIT_CONFIG_ERROR
